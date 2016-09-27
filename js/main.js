1dbe7dd758d35ad0acf695eaee4b566985e23ef2
artjynx <tina@artjynx.com>
2016-09-27 12:51:46 -0500

    created other files

---
+++ js/main.js
@@ -0,0 +1,43 @@
+///////////////////////////////////////////////////////
+///////////RECIPE FORM/////////////////////////////////
+///////////////////////////////////////////////////////
+
+//Create Recipe Object array that includes
+//Title:
+//Description:
+//Author:
+//completeIngredient Array: [Amount[i], Measurment[i], ingredient[i]]
+//Amount Array: []
+//Measurment Array: [];
+//Ingredient Array: [];
+//ingredientImage: Will include a file upload;
+
+
+
+
+//form input string for recipe header title
+//input string for recipe description
+//input for author or original location for recipe
+
+//input integers for recipe servings
+//input intgers for calories per serving
+
+//declare array for holding amounts
+//declare array for holding labels (tsp, cups, lemons...etc)
+
+//allow for a add ingredient push to the array
+//allow for a deleting of array capture current ID of the array to remove
+
+//allow for editing of a current existing array of ingredients and saving new element within the array
+
+//declare array for instructions
+
+//allow for a add instruction push to the array
+//allow for a deleting of an instruction array capture current ID of the array to remove
+
+//allow for editing of a current existing array of instructions and saving new element within the array
+
+//allow for addition of image and caption to each instruction include alt text string input for image
+
+//create button to submit data to a database and save it
+//allow for a publish boolean to allow for inclusion in a template to be published.
