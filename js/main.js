256ea7d91d6c02392d1fc1573834dcc6271f22f9
artjynx <tina@artjynx.com>
2016-09-29 10:21:05 -0500

    added new files and updated html

--- js/main.js
+++ js/main.js
@@ -11,7 +11,7 @@
 
  
 
-  
+
 
 ///////////////////////////////////////////////////////
 ///////////RECIPE FORM/////////////////////////////////
@@ -21,38 +21,52 @@
 //Title:
 //Description:
 //Author:
-//completeIngredient Array: [Amount[i], Measurment[i], ingredient[i]]
-//Amount Array: []
-//Measurment Array: [];
-//Ingredient Array: [];
-//ingredientImage: Will include a file upload;
+//Ingredient Object Array:[]
+//HeaderImg:
+//create an ingredient object using this
+//Ingredient Array of Objects: [Amount, Measurment, Label]
+//Instruction Object Array: [direction, Image]
+
+///////////////////////////////////////////////////////
+////////////////////HTML///////////////////////////////
+///////////////////////////////////////////////////////
+//Three pages exist one for listing all the recipes and allows for options to edit, delete, create
+//Second Page is for editing/creating a recipe
+//Third page is for viewing a single recipe
+
+///////////////////////////////////////////////////////
+///////////RECIPE SINGLE DISPLAY PAGE//////////////////
+///////////////////////////////////////////////////////
+//Once linked to the javascript looks for a recipe ID to query Firebase for a recipe to display
+//Populates the HTML with the recipe entry info
+
+///////////////////////////////////////////////////////
+///////////RECIPE LIST PAGE////////////////////////////
+///////////////////////////////////////////////////////
+//Query Firebase for existing recipes and display on page
+//for each recipe, display title and options for editing/deleting
+//EDIT Recipe -  link to other page and pass the ID of the recipe to display the selected recipe
+//DELETE Recipe - Listener and then when clicked use Firebase to delete that entry and refresh page with new list
+//VIEW a Recipe - links to a page with the ID of the Recipe passed into it for display - THere will be an edit button which can pass into the edit page
+//CREATE a new recipe - passes a null id to the edit page which then creates a blank form, once filled and submited will tell firebase to create a new entry
 
 
+///////////////////////////////////////////////////////
+///////////RECIPE EDIT/CREATE PAGE/////////////////////
+///////////////////////////////////////////////////////
+//Determine if the page is to edit an existing recipe (does it have an ID?)
+//If there is no ID (Create button will pass a null ID) create an empty for with a new ID number to push to Firebase once created
+//Title, Author, Description and header image will be simple text/upload of a file and required
+//ingredients will be added one at a time
+//Instructions will be added one at a time
+//listener for adding instructions or ingredients
+//listener for submitting and uploading to firebase and updating the database
 
 
-//form input string for recipe header title
-//input string for recipe description
-//input for author or original location for recipe
+//listener for editing/deleting image
+//function for uploading image to firebase and returning a URL
+//function for deleting and image
+//create function for listing all recipes
+//create a function for pulling a recipe and displaying it in a template
+//create a function for pulling a recipe and populating the recipe form 
 
-//input integers for recipe servings
-//input intgers for calories per serving
-
-//declare array for holding amounts
-//declare array for holding labels (tsp, cups, lemons...etc)
-
-//allow for a add ingredient push to the array
-//allow for a deleting of array capture current ID of the array to remove
-
-//allow for editing of a current existing array of ingredients and saving new element within the array
-
-//declare array for instructions
-
-//allow for a add instruction push to the array
-//allow for a deleting of an instruction array capture current ID of the array to remove
-
-//allow for editing of a current existing array of instructions and saving new element within the array
-
-//allow for addition of image and caption to each instruction include alt text string input for image
-
-//create button to submit data to a database and save it
-//allow for a publish boolean to allow for inclusion in a template to be published.
